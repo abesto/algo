bc72526571f2e424ebf593dc40186c98ad468e45
Zoltán Nagy <abesto@abesto.net>
2011-11-09 00:17:40 +0100

    Use minified vendor packages

--- public/javascripts/build.js
+++ public/javascripts/build.js
@@ -3,9 +3,13 @@
     baseUrl: ".",
     dir: "./",
 	optimize: "none",
+	paths: {
+		'vendor/jquery': 'vendor/jquery.min',
+		'vendor/underscore': 'vendor/underscore.min'
+	},
     modules: [
 	        {
-	            name: "main-built",
+	            name: "main",
 				include: ["main", "app/common/i18n/Updaters/SimpleUpdater", "app/common/i18n/Updaters/JqueryEventUpdater", "app/common/i18n/DataProviders/AsyncJsonDataProvider"]
 	        }
     ]
