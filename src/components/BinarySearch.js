eabd194f435aa69ced57ff89431763abec2313ac
Zoltan Nagy <abesto0@gmail.com>
2018-05-23 14:40:13 +0200

    Remove unused import

--- src/components/BinarySearch.js
+++ src/components/BinarySearch.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import _ from 'lodash'
 
 // React components, containers
 import Inspector from '../containers/Inspector.js'
