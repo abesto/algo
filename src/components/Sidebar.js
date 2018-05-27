421eabbeb9b0d76f348f30fe78f29cd8ca74ed7b
Zoltan Nagy <abesto0@gmail.com>
2018-05-27 19:18:56 +0200

    Casing.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import * as routes from '../constants/Routes'
+import * as Routes from '../constants/Routes'
 import '../styles/Sidebar.css'
 
 const Sidebar = () =>
   <nav className="Sidebar">
     <ul>
-      <li className="logo"><Link to={routes.LANDING}>algo.abesto.net</Link></li>
-      <li><Link to={routes.LINEAR_SEARCH}>Linear search</Link></li>
-      <li><Link to={routes.BINARY_SEARCH}>Binary search</Link></li>
+      <li className="logo"><Link to={Routes.LANDING}>algo.abesto.net</Link></li>
+      <li><Link to={Routes.LINEAR_SEARCH}>Linear search</Link></li>
+      <li><Link to={Routes.BINARY_SEARCH}>Binary search</Link></li>
     </ul>
   </nav>
 
